54da93c3b613bda62180ba6c67f96bb7b93fbffb
Rasmus Lind <rasmuslindnyberg@gmail.com>
2020-11-09 13:01:06 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace multifabriken_Muzze1994
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
